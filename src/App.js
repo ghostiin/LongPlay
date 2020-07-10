9058d6582be3ff610a07884bdf9949fd42b398eb
SH L <lsway1998@gmail.com>
2020-07-11 01:32:25 +0800

    old:v0.9

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { GlobalStyle } from './theme/globalStyles';
 import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalFonts } from './assets/fonts/fonts';
 import history from './history';
+
 function App() {
 	return (
 		<Router history={history}>
